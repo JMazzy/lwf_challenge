e5c6aac516f8d79914071bfa9a9b12107f0333fa
Joshua Masland-Sarani <j.masland@gmail.com>
2016-06-27 15:41:27 -0400

    basic functionality implemented

--- app/controllers/shipping_addresses_controller.rb
+++ app/controllers/shipping_addresses_controller.rb
@@ -7,4 +7,13 @@ class ShippingAddressesController < ApplicationController
   def new
   end
 
+  def create
+    file_data = params["address_file"]
+
+    ShippingAddress.create_from_file( file_data.tempfile,
+                                      file_data.original_filename )
+
+    redirect_to :shipping_addresses
+  end
+
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -4,4 +4,15 @@ class ShippingAddress < ActiveRecord::Base
 
   validates :customer_id, presence: true
   validates :address_id, presence: true
+
+  def self.create_from_file(file, name)
+    contents = File.readlines(file).map { |line| line.strip.split(",") }
+    contents.shift
+
+    contents.each do |line|
+      customer = Customer.create( first_name: line[0], last_name: line[1] )
+      address = Address.create( address: line[2], city: line[3], state: line[4], zip: line[5] )
+      ShippingAddress.create( customer_id: customer.id, address_id: address.id )
+    end
+  end
 end
